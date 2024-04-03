9c1fea3c2d1d9cb637489ef997724e434500e517
chuntaojun <liaochuntao@live.com>
2024-04-03 11:17:13 +0800

    feat:support polaris lane router

--- trpc-configcenter/trpc-configcenter-open-polaris/src/test/java/com/tencent/trpc/configcenter/polaris/PolarisConfigurationLoaderTest.java
+++ trpc-configcenter/trpc-configcenter-open-polaris/src/test/java/com/tencent/trpc/configcenter/polaris/PolarisConfigurationLoaderTest.java
@@ -11,8 +11,13 @@
 
 package com.tencent.trpc.configcenter.polaris;
 
+import com.tencent.polaris.configuration.api.core.ChangeType;
+import com.tencent.polaris.configuration.api.core.ConfigFileChangeListener;
 import com.tencent.polaris.configuration.api.core.ConfigFileService;
 import com.tencent.polaris.configuration.api.core.ConfigKVFile;
+import com.tencent.polaris.configuration.api.core.ConfigKVFileChangeEvent;
+import com.tencent.polaris.configuration.api.core.ConfigKVFileChangeListener;
+import com.tencent.polaris.configuration.api.core.ConfigPropertyChangeInfo;
 import com.tencent.polaris.configuration.client.internal.ConfigYamlFile;
 import com.tencent.trpc.core.common.config.PluginConfig;
 import com.tencent.trpc.core.configcenter.ConfigurationEvent;
@@ -22,11 +27,16 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mockito;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class PolarisConfigurationLoaderTest {
 
@@ -99,18 +109,165 @@ public class PolarisConfigurationLoaderTest {
     }
 
     @Test
-    public void testAddListener() {
-        ConfigurationListener listener = new ConfigurationListener() {
-            @Override
-            public void onChange(ConfigurationEvent event) throws ConfigCenterException {
+    public void testAddListener() throws Exception {
+        CountDownLatch latch = new CountDownLatch(1);
+        AtomicReference<ConfigurationEvent> ref = new AtomicReference<>();
 
-            }
+        ConfigurationListener listener = event -> {
+            ref.set(event);
+            latch.countDown();
         };
 
         PolarisConfigurationLoader loader = mockLoader();
 
+        Map<String, ConfigPropertyChangeInfo> changeInfos = new HashMap<>();
+        changeInfos.put("user.name", new ConfigPropertyChangeInfo("user.name", "", "polaris", ChangeType.ADDED));
+        ConfigKVFileChangeEvent changeEvent = new ConfigKVFileChangeEvent(changeInfos);
+
+        ConfigFileService fetcher = loader.getFetcher();
+        Mockito.when(fetcher.getConfigPropertiesFile("default", mockGroup, mockFile)).thenReturn(new MockConfigKVFile(changeEvent));
 
         loader.addListener(listener);
+        latch.await(1, TimeUnit.SECONDS);
+
+        Assert.assertNotNull(ref.get());
+        ConfigurationEvent event = ref.get();
+        Assert.assertEquals(event.getKey(), "user.name");
+        Assert.assertEquals(event.getValue(), "polaris");
+        Assert.assertEquals(event.getType(), ChangeType.ADDED.name());
+    }
+
+    private static class MockConfigKVFile implements ConfigKVFile {
+
+        private final ConfigKVFileChangeEvent event;
+
+        private MockConfigKVFile(ConfigKVFileChangeEvent event) {
+            this.event = event;
+        }
+
+        @Override
+        public String getProperty(String key, String defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Integer getIntProperty(String key, Integer defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Long getLongProperty(String key, Long defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Short getShortProperty(String key, Short defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Float getFloatProperty(String key, Float defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Double getDoubleProperty(String key, Double defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Byte getByteProperty(String key, Byte defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Boolean getBooleanProperty(String key, Boolean defaultValue) {
+            return null;
+        }
+
+        @Override
+        public String[] getArrayProperty(String key, String delimiter, String[] defaultValue) {
+            return new String[0];
+        }
+
+        @Override
+        public <T extends Enum<T>> T getEnumProperty(String key, Class<T> enumType, T defaultValue) {
+            return null;
+        }
+
+        @Override
+        public <T> T getJsonProperty(String key, Class<T> clazz, T defaultValue) {
+            return null;
+        }
+
+        @Override
+        public <T> T getJsonProperty(String key, Type typeOfT, T defaultValue) {
+            return null;
+        }
+
+        @Override
+        public Set<String> getPropertyNames() {
+            return null;
+        }
+
+        @Override
+        public void addChangeListener(ConfigKVFileChangeListener listener) {
+            listener.onChange(event);
+        }
+
+        @Override
+        public void removeChangeListener(ConfigKVFileChangeListener listener) {
+
+        }
+
+        @Override
+        public String getContent() {
+            return null;
+        }
+
+        @Override
+        public <T> T asJson(Class<T> objectType, T defaultValue) {
+            return null;
+        }
+
+        @Override
+        public <T> T asJson(Type typeOfT, T defaultValue) {
+            return null;
+        }
+
+        @Override
+        public boolean hasContent() {
+            return false;
+        }
+
+        @Override
+        public String getMd5() {
+            return null;
+        }
+
+        @Override
+        public void addChangeListener(ConfigFileChangeListener listener) {
+        }
+
+        @Override
+        public void removeChangeListener(ConfigFileChangeListener listener) {
+
+        }
+
+        @Override
+        public String getNamespace() {
+            return null;
+        }
+
+        @Override
+        public String getFileGroup() {
+            return null;
+        }
+
+        @Override
+        public String getFileName() {
+            return null;
+        }
     }
 
 }
