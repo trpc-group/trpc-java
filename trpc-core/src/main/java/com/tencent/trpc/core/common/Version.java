77ed70a580538c5a851dcdbdbdf52fdc6e3c50b1
wardseptember <wardseptember@163.com>
2024-09-30 10:57:50 +0800

    feat: release 1.3.0 version

--- trpc-core/src/main/java/com/tencent/trpc/core/common/Version.java
+++ trpc-core/src/main/java/com/tencent/trpc/core/common/Version.java
@@ -31,7 +31,7 @@ public class Version {
      * VERSION: Do not modify the line number of this line. If you want to modify it, be sure to change deploy.sh at
      * the same time.
      */
-    public static final String VERSION = "v1.2.0";
+    public static final String VERSION = "v1.3.0";
     public static final String SNAPSHOT_VERSION = VERSION + VERSION_SUFFIX;
     /**
      * IS_FORMAL_VERSION: Do not modify the line number of this line. If you want to modify it, be sure to change
